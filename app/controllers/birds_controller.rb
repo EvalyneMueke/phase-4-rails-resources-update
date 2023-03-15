b3cd82727975c2508ca909c90556d6356e320049
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-03-15 12:48:39 +0300

    Update

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,32 @@
 class BirdsController < ApplicationController
 
+# PATCH /birds/:id
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  def bird_params
+    params.permit(:name, :species, :likes)
+  end
+
+
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+
   # GET /birds
   def index
     birds = Bird.all
@@ -27,5 +54,7 @@ class BirdsController < ApplicationController
   def bird_params
     params.permit(:name, :species)
   end
+
+
 
 end
